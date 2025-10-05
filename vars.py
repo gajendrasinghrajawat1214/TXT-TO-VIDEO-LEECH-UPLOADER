c2e00567b535d5c3844cc53fb9715688b034bc55
gajendrasinghrajawat1214 <gajendrasinghrajawat1214@gmail.com>
2025-10-05 12:55:36 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "23065170"))
+API_HASH = environ.get("API_HASH", "7d043ca5d09ffc457879b47af9ca77cd")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8337571581:AAF9Nn-dyuQ29O4P-fchnWBfpjxKd8tTRqA")
 
 # Force Subscribe Configuration
 FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "roxybasicneedbot1")  # Channel username without @, 
@@ -18,6 +18,7 @@ OWNER_ID = int(environ.get("OWNER_ID", ""))
 
 # Database URL (if you want to add database support later)
 DATABASE_URL = environ.get("DATABASE_URL", "")
+
 
 
 
